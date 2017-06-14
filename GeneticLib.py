f5c9da96e5e5530a091a7719950813e719e355f8
Allan Targino <altargin@microsoft.com>
2017-06-13 21:31:35 -0300

    CrossoverTechniques

--- GeneticLib.py
+++ GeneticLib.py
@@ -1,9 +1,20 @@
+from enum import Enum
+
 class Solver(object):
 
-    def __init__(self, maxGenerations, mutationRate, fitnessFunc):
+    def __init__(self, maxGenerations, mutationRate, fitnessFunc, crossoverTechnique):
         self.maxGenerations = maxGenerations
         self.mutationRate = mutationRate
         self.fitnessFunc = fitnessFunc
+        self.crossoverTechnique = crossoverTechnique
 
     def solve(self):
         return self.fitnessFunc()
+
+# based on https://en.wikipedia.org/wiki/Crossover_(genetic_algorithm)#Techniques
+class CrossoverTechnique(Enum):
+    SinglePoint = 1
+    TwoPoint = 2
+    Uniform = 3 
+    HalfUniform = 4
+    ThreeParent = 5
--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
-from GeneticLib import Solver
+from GeneticLib import CrossoverTechnique, Solver
+
+verbose = False
 
 maxGenerations = 100
 mutationRate = 0.1
+populationInit = 50
+crossoverChild = 10
+crossoverTechnique = CrossoverTechnique.TwoPoint
+
 def fitnessFunc():
     return 1
 
